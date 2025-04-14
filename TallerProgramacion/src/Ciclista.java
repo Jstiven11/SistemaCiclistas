a4fee008243285515b4affa1c51e49a9bb7ad9d7
Jstiven11 <141979717+Jstiven11@users.noreply.github.com>
2025-04-13 23:06:50 -0500

    Se agregan las clases restantes, nos ayuda validando porque no nos permite realizar el pull request tal vez sea por la version 8.2 de netbeans

---
+++ TallerProgramacion/src/Ciclista.java
@@ -0,0 +1,26 @@
+public abstract class Ciclista {
+    private int identificador;
+    private String nombre;
+    private int tiempoAcumulado;
+
+    public Ciclista(int identificador, String nombre) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.tiempoAcumulado = 0;
+    }
+
+    public int getIdentificador() { return identificador; }
+    public void setIdentificador(int identificador) { this.identificador = identificador; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getTiempoAcumulado() { return tiempoAcumulado; }
+    public void setTiempoAcumulado(int tiempoAcumulado) { this.tiempoAcumulado = tiempoAcumulado; }
+
+    public void imprimirDatos() {
+        System.out.println("ID: " + identificador + ", Nombre: " + nombre + ", Tiempo: " + tiempoAcumulado + " min");
+    }
+
+    public abstract String imprimirTipo();
+}
